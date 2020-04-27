06bbd384a1c6c7041967181b088f2664b7b0c36d
Winda <azis.widad@gmail.com>
2020-04-27 12:29:47 +0200

    Exercice compte bancaire

---
+++ classes/CompteBancaire.py
@@ -0,0 +1,16 @@
+class CompteBancaire(object):
+    _nom = "Dupont"
+    _solde = 1000
+    def __init__(self, nom, solde):
+        self._nom = nom
+        self._solde = solde
+
+    def depot(self, nb):
+        self._solde += nb
+
+    def retrait(self, nb):
+        self._solde -= nb
+    
+    def affichage(self):
+        print("le solde du compte de " + self._nom + " est de ")
+        print(self._solde)
--- exercice.py
+++ exercice.py
@@ -1,3 +1,19 @@
 #! /usr/bin/pythonw
+from classes.CompteBancaire import CompteBancaire
 test = 'test'
 print(test)
+
+a = 5
+b = 10
+if a > 0:
+    a += 3
+    print("a =",a,"et b =",b)
+
+mot = "lac"
+mot = mot[-1]
+print(mot)
+
+#Exercice compte bancaire
+compte1 = CompteBancaire("Winda", 2000)
+compte1.depot(900)
+compte1.affichage()
